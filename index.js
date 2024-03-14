e14b34509665db6cd6f62209f46ab9ad7ccace18
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-03-14 11:27:24 +0100

    change CSS and add JS code Kana part

--- index.js
+++ index.js
@@ -181,8 +181,47 @@ const changeColorRanking = document.getElementsByTagName("li");
 
 for (let i = 0; i < changeColorRanking.length; i++) {
     changeColorRanking[i].addEventListener('click', function () {
-        this.style.backgroundColor = 'red';
-
+        if (this.style.backgroundColor === '') {
+            this.style.backgroundColor = 'grey';
+        }
+        else {
+            this.style.backgroundColor = '';
+        }
     });
 }
+
+// ----------------------Function star  (WIP) Kana------------------
+//Star maker
+function starMaker(n) {
+    var star = document.createElement("div");
+    star.className = "star";
+    star.textContent = "★";
+    for (var i = 0; i < n; i++) {
+        starSet(star);
+    }
+}
+
+//Setting start
+function starSet(clone) {
+    var starClone = clone.cloneNode(true);
+    var starStyle = starClone.style;
+
+    //Star position（left）,（animation-delay）、size（font-size）random decision 
+    starStyle.left = 100 * Math.random() + "%";
+    starStyle.animationDelay = 8 * Math.random() + "s";
+    starStyle.fontSize = ~~(50 * Math.random() + 20) + "px";
+    document.body.appendChild(starClone);
+
+    //create new starts when it ends 
+    starClone.addEventListener("animationend", function () {
+        this.parentNode.removeChild(this);
+        var star = document.createElement("div");
+        star.className = "star";
+        star.textContent = "★";
+        starSet(star);
+    }, false)
+}
+
+//50 stars 
+starMaker(50)
 
