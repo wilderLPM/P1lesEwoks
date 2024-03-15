66870a9d56f147e21e6d2a54e281e74419201da1
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-03-15 09:39:06 +0100

    Base de travail propre

--- index.js
+++ index.js
@@ -1,97 +1,124 @@
+//-----------CODE DE LEO-----------------
+
+const correctButtons = document.getElementsByClassName("correct");
+const incorrectButtons = document.getElementsByClassName("incorrect");
+const correctButtonsArray = Array.from(correctButtons);
+const incorrectButtonsArray = Array.from(incorrectButtons);
+const questions = document.querySelectorAll(".question-text");
+const buttons = document.getElementsByClassName("answer-button"); // VérifieR qu'il s'agit bien d'un boutton (et non une classe qui contient 'button')
+
+const vrai = Array.from(document.getElementsByClassName("true"));
+const faux = Array.from(document.getElementsByClassName("false"));
+const userChoice = vrai.concat(faux);
+
+console.log("Correct buttons:", correctButtons);
+console.log("Incorrect buttons:", incorrectButtons);
+console.log("Questions:", questions);
+console.log(typeof correctButtons);
+console.log(typeof incorrectButtons);
+
+// MET EN EVIDENCE LE BOUTON QUAND ON LE SURVOLE
+for (const button of faux) {
+    if (button instanceof HTMLButtonElement && !button.disabled) {
+        button.addEventListener("mouseenter", function changeBC() {
+            this.style.backgroundColor = "#ff7556";
+        });
+        button.addEventListener("mouseleave", function changeBC() {
+            this.style.backgroundColor = "#F26444";
+        });
+    }
+}
+for (const button of vrai) {
+    if (button instanceof HTMLButtonElement && !button.disabled) {
+        button.addEventListener("mouseenter", function changeBC() {
+            this.style.backgroundColor = "#12c794";
+        });
+        button.addEventListener("mouseleave", function changeBCback() {
+            this.style.backgroundColor = "#0aa67a";
+        });
+    }
+}
+
+// WHEN USER CHOOSES CORRECT ANSWER
+correctButtonsArray.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        questions[index].style.backgroundColor = "#0aa67a";
+        correctButtonsArray[index].disabled = true;
+        incorrectButtonsArray[index].disabled = true;
+    });
+});
+
+// WHEN USER CHOOSES INCORRECT ANSWER
+incorrectButtonsArray.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        questions[index].style.backgroundColor = "#F26444";
+        correctButtonsArray[index].disabled = true;
+        incorrectButtonsArray[index].disabled = true;
+    });
+});
+
+
+// GRISE LE BOUTON INCORRECT QUAND USER CHOISIT UNE REPONSE
+for (i in faux) {
+    if (
+        buttons[i] instanceof HTMLButtonElement &&
+        (buttons[i].className === "true" || buttons[i].className === "false")
+    ) {
+        buttons[i].addEventListener("click", function () {
+            if (buttons[i].className === "true") {
+                incorrectButtons[i].style.backgroundColor = "#0aa67ab5";
+            } else {
+                incorrectButtons[i].style.backgroundColor = "#f2644492";
+            }
+        });
+    }
+}
+/* #f2644492 */ // QUAND FAUX EST INCORRECT
+/* #0aa67ab5  */ // QUAND VRAI EST INCORRECT
+
+userChoice.forEach(function (button) {
+    button.addEventListener("click", function () {
+        const parentDiv = button.parentElement; // Récupérez l'élément parent du bouton
+        const incorrectButton = parentDiv.querySelector(".incorrect"); // Sélectionnez le bouton incorrect dans le même conteneur que le bouton cliqué
+        const correctButton = parentDiv.querySelector(".correct");
+        if (incorrectButton.classList.contains("true")) {
+            // Vérifiez si le bouton incorrect a la classe "vrai"
+            incorrectButton.style.backgroundColor = "#0aa67ab5"; // Modifiez la couleur de fond du bouton incorrect
+            correctButton.style.backgroundColor = "#ff7556";
+        } else {
+            // Si le bouton incorrect a la classe "faux"
+            incorrectButton.style.backgroundColor = "#f2644492"; // Modifiez la couleur de fond du bouton incorrect
+            correctButton.style.backgroundColor = "#12c794";
+        }
+    });
+});
+
+//--------------CODE DE THIEN-----------------
+
 let header_screen = document.getElementById("homepage");
 let questions_screen = document.getElementById("questions_screen");
-let questions_screen2 = document.getElementById("questions_screen2");
-let questions_screen3 = document.getElementById("questions_screen3");
-let questions_screen4 = document.getElementById("questions_screen4");
-let questions_screen5 = document.getElementById("questions_screen5");
-let questions_screen6 = document.getElementById("questions_screen6");
-let questions_screen7 = document.getElementById("questions_screen7");
-let questions_screen8 = document.getElementById("questions_screen8");
-let questions_screen9 = document.getElementById("questions_screen9");
-let questions_screen10 = document.getElementById("questions_screen10");
 let result_screen = document.getElementById("result_screen");
 
 function startQuestions() {
-  header_screen.style.display = "none";
-  questions_screen.style.display = "block";
+    header_screen.style.display = "none"; // La classe a changé
+    questions_screen.style.display = "block";
 }
 let btn_start = document.getElementById("btn_start");
 btn_start.addEventListener("click", startQuestions);
 
-function startQuestions2() {
-  questions_screen.style.display = "none";
-  questions_screen2.style.display = "block";
-}
-let btn_next = document.getElementById("btn_next");
-btn_next.addEventListener("click", startQuestions2);
 
-function startQuestions3() {
-  questions_screen2.style.display = "none";
-  questions_screen3.style.display = "block";
-}
-let btn_next2 = document.getElementById("btn_next2");
-btn_next2.addEventListener("click", startQuestions3);
-
-function startQuestions4() {
-  questions_screen3.style.display = "none";
-  questions_screen4.style.display = "block";
-}
-let btn_next3 = document.getElementById("btn_next3");
-btn_next3.addEventListener("click", startQuestions4);
-
-function startQuestions5() {
-  questions_screen4.style.display = "none";
-  questions_screen5.style.display = "block";
-}
-let btn_next4 = document.getElementById("btn_next4");
-btn_next4.addEventListener("click", startQuestions5);
-
-function startQuestions6() {
-  questions_screen5.style.display = "none";
-  questions_screen6.style.display = "block";
-}
-let btn_next5 = document.getElementById("btn_next5");
-btn_next5.addEventListener("click", startQuestions6);
-
-function startQuestions7() {
-  questions_screen6.style.display = "none";
-  questions_screen7.style.display = "block";
-}
-let btn_next6 = document.getElementById("btn_next6");
-btn_next6.addEventListener("click", startQuestions7);
-
-function startQuestions8() {
-  questions_screen7.style.display = "none";
-  questions_screen8.style.display = "block";
-}
-let btn_next7 = document.getElementById("btn_next7");
-btn_next7.addEventListener("click", startQuestions8);
-
-function startQuestions9() {
-  questions_screen8.style.display = "none";
-  questions_screen9.style.display = "block";
-}
-let btn_next8 = document.getElementById("btn_next8");
-btn_next8.addEventListener("click", startQuestions9);
-
-function startQuestions10() {
-  questions_screen9.style.display = "none";
-  questions_screen10.style.display = "block";
-}
-let btn_next9 = document.getElementById("btn_next9");
-btn_next9.addEventListener("click", startQuestions10);
 
 function startResult() {
-  questions_screen10.style.display = "none";
-  result_screen.style.display = "block";
+    questions_screen10.style.display = "none";
+    result_screen.style.display = "block";
 }
-let btn_next10 = document.getElementById("btn_next10");
+let btn_next10 = document.getElementById("btn_next10"); // A Modifier
 btn_next10.addEventListener("click", startResult);
 // ------------- PLAY AGAIN ----------------
 
 function backHome() {
-  result_screen.style.display = "none";
-  homepage.style.display = "block";
+    result_screen.style.display = "none";
+    homepage.style.display = "block";
 }
 let playagain = document.getElementById("playagain");
 playagain.addEventListener("click", backHome);
@@ -136,84 +163,7 @@ playagain.addEventListener("click", backHome);
   */
 // ----------- CODE TEST -------------
 
-//-----------CODE DE LEO-----------------
-const correctButtons = document.getElementsByClassName("correct");
-const incorrectButtons = document.getElementsByClassName("incorrect");
-const questions = document.querySelectorAll(".question>p");
-const buttons = document.getElementsByTagName("button"); // VérifieR qu'il s'agit bien d'un boutton (et non une classe qui contient 'button')
-/* const vrai = document.getElementsByClassName("vrai");
-const faux = document.getElementsByClassName("faux"); */
-const vrai = Array.from(document.getElementsByClassName("vrai"));
-const faux = Array.from(document.getElementsByClassName("faux"));
-const userChoice = vrai.concat(faux);
 
-// MET EN EVIDENCE LE BOUTON QUAND ON LE SURVOLE
-for (i in faux) {
-  if (faux[i] instanceof HTMLButtonElement) {
-    faux[i].addEventListener("mouseenter", function changeBC() {
-      this.style.backgroundColor = "#ff7556";
-    });
-    faux[i].addEventListener("mouseleave", function changeBC() {
-      this.style.backgroundColor = "#F26444";
-    });
-  }
-}
-for (i in vrai) {
-  if (vrai[i] instanceof HTMLButtonElement) {
-    vrai[i].addEventListener("mouseenter", function changeBC() {
-      this.style.backgroundColor = "#12c794";
-    });
-    vrai[i].addEventListener("mouseleave", function changeBCback() {
-      this.style.backgroundColor = "#0aa67a";
-    });
-  }
-}
-// WHEN USER CHOOSES CORRECT ANSWER
-for (let i = 0; i < correctButtons.length; i++) {
-  correctButtons[i].addEventListener("click", function () {
-    questions[i].style.backgroundColor = "#0aa67a";
-  });
-}
-// WHEN USER CHOOSES INCORRECT ANSWER
-for (let i = 0; i < incorrectButtons.length; i++) {
-  incorrectButtons[i].addEventListener("click", function () {
-    questions[i].style.backgroundColor = "#F26444";
-  });
-}
-// GRISE LE BOUTON INCORRECT QUAND USER CHOISIT UNE REPONSE
-for (i in faux) {
-  if (
-    buttons[i] instanceof HTMLButtonElement &&
-    (buttons[i].className === "vrai" || buttons[i].className === "faux")
-  ) {
-    buttons[i].addEventListener("click", function () {
-      if (buttons[i].className === "vrai") {
-        incorrectButtons[i].style.backgroundColor = "#0aa67ab5";
-      } else {
-        incorrectButtons[i].style.backgroundColor = "#f2644492";
-      }
-    });
-  }
-}
-/* #f2644492 */ // QUAND FAUX EST INCORRECT
-/* #0aa67ab5  */ // QUAND VRAI EST INCORRECT
-
-userChoice.forEach(function (button) {
-  button.addEventListener("click", function () {
-    const parentDiv = button.parentElement; // Récupérez l'élément parent du bouton
-    const incorrectButton = parentDiv.querySelector(".incorrect"); // Sélectionnez le bouton incorrect dans le même conteneur que le bouton cliqué
-    const correctButton = parentDiv.querySelector(".correct");
-    if (incorrectButton.classList.contains("vrai")) {
-      // Vérifiez si le bouton cliqué a la classe "vrai"
-      incorrectButton.style.backgroundColor = "#0aa67ab5"; // Modifiez la couleur de fond du bouton incorrect
-      correctButton.style.backgroundColor = "#ff7556";
-    } else {
-      // Si le bouton cliqué a la classe "faux"
-      incorrectButton.style.backgroundColor = "#f2644492"; // Modifiez la couleur de fond du bouton incorrect
-      correctButton.style.backgroundColor = "#12c794";
-    }
-  });
-});
 
 let avatarButton1 = document.getElementsByClassName("avatar1");
 let avatarButton2 = document.getElementsByClassName("avatar2");
@@ -221,23 +171,23 @@ let avatarButton3 = document.getElementsByClassName("avatar3");
 let avatarButton4 = document.getElementsByClassName("avatar4");
 let lastClickedAvatar = null;
 function configureAvatarButton(avatarButtons) {
-  for (let i = 0; i < avatarButtons.length; i++) {
-    avatarButtons[i].addEventListener("click", function (event) {
-      event.preventDefault();
-      // Réinitialise les styles du dernier bouton cliqué (s'il existe)
-      if (lastClickedAvatar) {
-        lastClickedAvatar.style.transform = "scale(1)";
-        lastClickedAvatar.style.backgroundColor = "";
-        lastClickedAvatar.style.borderRadius = "";
-      }
-      // Met à jour les styles du bouton actuel
-      this.style.transform = "scale(1.2)";
-      this.style.backgroundColor = "#f28c11";
-      this.style.borderRadius = "50%";
-      // Met à jour la référence du dernier bouton cliqué
-      lastClickedAvatar = this;
-    });
-  }
+    for (let i = 0; i < avatarButtons.length; i++) {
+        avatarButtons[i].addEventListener("click", function (event) {
+            event.preventDefault();
+            // Réinitialise les styles du dernier bouton cliqué (s'il existe)
+            if (lastClickedAvatar) {
+                lastClickedAvatar.style.transform = "scale(1)";
+                lastClickedAvatar.style.backgroundColor = "";
+                lastClickedAvatar.style.borderRadius = "";
+            }
+            // Met à jour les styles du bouton actuel
+            this.style.transform = "scale(1.2)";
+            this.style.backgroundColor = "#f28c11";
+            this.style.borderRadius = "50%";
+            // Met à jour la référence du dernier bouton cliqué
+            lastClickedAvatar = this;
+        });
+    }
 }
 configureAvatarButton(avatarButton1, 1);
 configureAvatarButton(avatarButton2, 2);
@@ -245,19 +195,19 @@ configureAvatarButton(avatarButton3, 3);
 configureAvatarButton(avatarButton4, 4);
 
 function validerPseudo() {
-  // Récupérer la valeur du pseudo
-  let pseudo = document.getElementById("pseudo").value;
+    // Récupérer la valeur du pseudo
+    let pseudo = document.getElementById("pseudo").value;
 
-  // Vérifier si le pseudo est vide
-  if (pseudo.trim() === "") {
-    alert("Veuillez saisir un pseudo");
-    return;
-  }
-  // Afficher le pseudo dans le résultat
-  document.getElementById("pseudoAffiche").innerText = pseudo;
+    // Vérifier si le pseudo est vide
+    if (pseudo.trim() === "") {
+        alert("Veuillez saisir un pseudo");
+        return;
+    }
+    // Afficher le pseudo dans le résultat
+    document.getElementById("pseudoAffiche").innerText = pseudo;
 
-  // Afficher le résultat
-  document.getElementById("resultat").style.display = "block";
+    // Afficher le résultat
+    document.getElementById("resultat").style.display = "block";
 }
 
 // ----------------------Function go to top page Kana------------------
@@ -266,7 +216,7 @@ const goTopageTop = document.getElementById("playagain");
 
 // This funtion works, back to top but too much
 goTopageTop.addEventListener("click", function () {
-  window.location.href = "#pageTop";
+    window.location.href = "#pageTop";
 });
 
 //If we will not go scroll page, this can be worked.
@@ -334,5 +284,11 @@ function starSet(clone) {
 }
 
 //50 stars 
-starMaker(50)
+/* starMaker(50) */
+
+// CODE POUR AFFICHER LE NUMERO DE LA QUESTION ACTUELLE
+let questionNumber = document.getElementsByClassName("question-number")[0];
+questionNumber.innerHTML = `Question n°${document.getElementsByClassName("enabled").length}`;
+// Il faut créer le code JS pour ajouter la classe "enabled" aux questions cards dont le display est block
+
 
---
+++ questionCards.js
@@ -0,0 +1,73 @@
+const questionCards = [
+    {
+        img: "images/baseball_kaleien.svg",
+        question: "Le match de baseball le plus long du monde à duré plus de 8h",
+        answer: "Lorem ipsum",
+        vrai: "correct",
+        faux: "incorrect"
+    },
+    {
+        img: "images/programing.svg",
+        question: "Le langage de programmation les plus rémunérateur en 2023 est le C# ",
+        answer: "",
+        vrai: "incorrect",
+        faux: "correct"
+    },
+    {
+        img: "images/solar-system.svg",
+        question: "La Terre est la 3ème planète la plus proche du Soleil",
+        answer: "",
+        vrai: "correct",
+        faux: "incorrect"
+    },
+    {
+        img: "images/french.svg",
+        question: "En France, il y a 15 jours feriés par an.",
+        answer: "",
+        vrai: "incorrect",
+        faux: "correct"
+    },
+    {
+        img: "images/Lyon-question.svg",
+        question: "Lyon est appelé ainsi depuis le 13e siècle",
+        answer: "",
+        vrai: "correct",
+        faux: "incorrect"
+    },
+    {
+        img: "images/cinema-question.svg",
+        question: "Titanic est le premier film a avoir été récompensé par 11 Oscars",
+        answer: "",
+        vrai: "incorrect",
+        faux: "correct"
+    },
+    {
+        img: "images/art-question.svg",
+        question: "Les joueurs de cartes de Cézanne est le tableau qui s'est vendu le plus cher, pour 250 millions USD",
+        answer: "",
+        vrai: "incorrect",
+        faux: "correct"
+    },
+    {
+        img: "images/pizza-question.svg",
+        question: `La "Pizza Margherita" a été créée en l'honneur de la ville Margherita`,
+        answer: "",
+        vrai: "incorrect",
+        faux: "correct"
+    },
+    {
+        img: "images/ewok-question.svg",
+        question: "Créé par George Lucas, l'ewok apparait pour la première fois dans Star Wars, épisode IV : Un nouvel espoir",
+        answer: "",
+        vrai: "incorrect",
+        faux: "correct"
+    },
+    {
+        img: "images/onepiece-question.svg",
+        question: "Le dernier volume de ONE PIECE, paru le 4 mars 2024 au Japon, est le n°108",
+        answer: "",
+        vrai: "correct",
+        faux: "incorrect"
+    },
+
+]
