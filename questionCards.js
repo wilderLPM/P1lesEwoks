bc558c648d3ac8e82b9c0582c04b7729a3568fff
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-03-20 15:36:55 +0100

    added alt key ans one answer

--- questionCards.js
+++ questionCards.js
@@ -1,73 +1,88 @@
 const questionCards = [
-    {
-        img: "images/baseball_kaleien.svg",
-        question: "Le match de baseball le plus long du monde à duré plus de 8h",
-        answer: "Lorem ipsum",
-        vrai: "correct",
-        faux: "incorrect"
-    },
-    {
-        img: "images/programing.svg",
-        question: "Le langage de programmation les plus rémunérateur en 2023 est le C# ",
-        answer: "",
-        vrai: "incorrect",
-        faux: "correct"
-    },
-    {
-        img: "images/solar-system.svg",
-        question: "La Terre est la 3ème planète la plus proche du Soleil",
-        answer: "",
-        vrai: "correct",
-        faux: "incorrect"
-    },
-    {
-        img: "images/french.svg",
-        question: "En France, il y a 15 jours feriés par an.",
-        answer: "",
-        vrai: "incorrect",
-        faux: "correct"
-    },
-    {
-        img: "images/Lyon-question.svg",
-        question: "Lyon est appelé ainsi depuis le 13e siècle",
-        answer: "",
-        vrai: "correct",
-        faux: "incorrect"
-    },
-    {
-        img: "images/cinema-question.svg",
-        question: "Titanic est le premier film a avoir été récompensé par 11 Oscars",
-        answer: "",
-        vrai: "incorrect",
-        faux: "correct"
-    },
-    {
-        img: "images/art-question.svg",
-        question: "Les joueurs de cartes de Cézanne est le tableau qui s'est vendu le plus cher, pour 250 millions USD",
-        answer: "",
-        vrai: "incorrect",
-        faux: "correct"
-    },
-    {
-        img: "images/pizza-question.svg",
-        question: `La "Pizza Margherita" a été créée en l'honneur de la ville Margherita`,
-        answer: "",
-        vrai: "incorrect",
-        faux: "correct"
-    },
-    {
-        img: "images/ewok-question.svg",
-        question: "Créé par George Lucas, l'ewok apparait pour la première fois dans Star Wars, épisode IV : Un nouvel espoir",
-        answer: "",
-        vrai: "incorrect",
-        faux: "correct"
-    },
-    {
-        img: "images/onepiece-question.svg",
-        question: "Le dernier volume de ONE PIECE, paru le 4 mars 2024 au Japon, est le n°108",
-        answer: "",
-        vrai: "correct",
-        faux: "incorrect"
-    },
-
-]
+  {
+    img: "images/baseball_kaleien.svg",
+    alt: "",
+    question: "Le match de baseball le plus long du monde à duré plus de 8h",
+    answer: "",
+    vrai: "correct",
+    faux: "incorrect",
+  },
+  {
+    img: "images/programing.svg",
+    alt: "",
+    question:
+      "Le langage de programmation les plus rémunérateur en 2023 est le C# ",
+    answer: "",
+    vrai: "incorrect",
+    faux: "correct",
+  },
+  {
+    img: "images/solar-system.svg",
+    alt: "",
+    question: "La Terre est la 3ème planète la plus proche du Soleil",
+    answer: "",
+    vrai: "correct",
+    faux: "incorrect",
+  },
+  {
+    img: "images/french.svg",
+    alt: "",
+    question: "En France, il y a 15 jours feriés par an.",
+    answer: "",
+    vrai: "incorrect",
+    faux: "correct",
+  },
+  {
+    img: "images/Lyon-question.svg",
+    alt: "",
+    question: "Lyon est appelé ainsi depuis le 13e siècle",
+    answer: "",
+    vrai: "correct",
+    faux: "incorrect",
+  },
+  {
+    img: "images/cinema-question.svg",
+    alt: "",
+    question:
+      "Titanic est le premier film a avoir été récompensé par 11 Oscars",
+    answer:
+      "Ben-Hur(1959) est le premier film à avoir gagné 11 Oscars, ce record sera égalé par Titanic(1997) et Le Seigneur des Anneaux : Le Retour du Roi(2003)",
+    vrai: "incorrect",
+    faux: "correct",
+  },
+  {
+    img: "images/art-question.svg",
+    alt: "",
+    question:
+      "Les joueurs de cartes de Cézanne est le tableau qui s'est vendu le plus cher, pour 250 millions USD",
+    answer: "",
+    vrai: "incorrect",
+    faux: "correct",
+  },
+  {
+    img: "images/pizza-question.svg",
+    alt: "",
+    question: `La "Pizza Margherita" a été créée en l'honneur de la ville Margherita`,
+    answer: "",
+    vrai: "incorrect",
+    faux: "correct",
+  },
+  {
+    img: "images/ewok-question.svg",
+    alt: "",
+    question:
+      "Créé par George Lucas, l'ewok apparait pour la première fois dans Star Wars, épisode IV : Un nouvel espoir",
+    answer: "",
+    vrai: "incorrect",
+    faux: "correct",
+  },
+  {
+    img: "images/onepiece-question.svg",
+    alt: "",
+    question:
+      "Le dernier volume de ONE PIECE, paru le 4 mars 2024 au Japon, est le n°108",
+    answer: "",
+    vrai: "correct",
+    faux: "incorrect",
+  },
+];
