4fa6eb5b45ea8a8eba5baad95f25abffa8e90062
Thien Nguyen <thien.ngu25@gmail.com>
2024-03-21 14:10:36 +0100

    modify avatar and css for media screen

---
+++ buttons.js
@@ -0,0 +1,80 @@
+const correctButtons = document.querySelector(".correct");
+const incorrectButtons = document.querySelector(".incorrect");
+const buttons = document.getElementsByClassName("answerButton");
+
+const vrai = Array.from(document.getElementsByClassName("true")); // Pas sûr que ce soit toujours utile
+const faux = Array.from(document.getElementsByClassName("false"));
+const userChoice = vrai.concat(faux);
+
+// MET EN EVIDENCE LE BOUTON QUAND ON LE SURVOLE
+for (const button of faux) {
+  if (button instanceof HTMLButtonElement && !button.disabled) {
+    button.addEventListener("mouseenter", function changeBC() {
+      this.style.backgroundColor = "#ff7556";
+    });
+    button.addEventListener("mouseleave", function changeBC() {
+      this.style.backgroundColor = "#F26444";
+    });
+  }
+}
+for (const button of vrai) {
+  if (button instanceof HTMLButtonElement && !button.disabled) {
+    button.addEventListener("mouseenter", function changeBC() {
+      this.style.backgroundColor = "#12c794";
+    });
+    button.addEventListener("mouseleave", function changeBCback() {
+      this.style.backgroundColor = "#0aa67a";
+    });
+  }
+}
+
+// WHEN USER CHOOSES CORRECT ANSWER
+correctButtons.addEventListener("click", () => {
+  questionText.style.backgroundColor = "#0aa67a";
+  questionText.innerText = questionCards.solution;
+  correctButtons.disabled = true;
+  incorrectButtons.disabled = true;
+});
+
+// WHEN USER CHOOSES INCORRECT ANSWER
+incorrectButtons.addEventListener("click", () => {
+  questionText.style.backgroundColor = "#F26444";
+  questionText.innerText = questionCards.solution;
+  correctButtons.disabled = true;
+  incorrectButtons.disabled = true;
+});
+
+// GRISE LE BOUTON INCORRECT QUAND USER CHOISIT UNE REPONSE
+for (i in faux) {
+  if (
+    buttons[i] instanceof HTMLButtonElement &&
+    (buttons[i].className === "true" || buttons[i].className === "false")
+  ) {
+    buttons[i].addEventListener("click", function () {
+      if (buttons[i].className === "true") {
+        incorrectButtons[i].style.backgroundColor = "#0aa67ab5";
+      } else {
+        incorrectButtons[i].style.backgroundColor = "#f2644492";
+      }
+    });
+  }
+}
+/* #f2644492 */ // QUAND FAUX EST INCORRECT
+/* #0aa67ab5  */ // QUAND VRAI EST INCORRECT
+
+userChoice.forEach(function (button) {
+  button.addEventListener("click", function () {
+    const parentDiv = button.parentElement; // Récupérez l'élément parent du bouton
+    const incorrectButton = parentDiv.querySelector(".incorrect"); // Sélectionnez le bouton incorrect dans le même conteneur que le bouton cliqué
+    const correctButton = parentDiv.querySelector(".correct");
+    if (incorrectButton.classList.contains("true")) {
+      // Vérifiez si le bouton incorrect a la classe "vrai"
+      incorrectButton.style.backgroundColor = "#0aa67ab5"; // Modifiez la couleur de fond du bouton incorrect
+      correctButton.style.backgroundColor = "#ff7556";
+    } else {
+      // Si le bouton incorrect a la classe "faux"
+      incorrectButton.style.backgroundColor = "#f2644492"; // Modifiez la couleur de fond du bouton incorrect
+      correctButton.style.backgroundColor = "#12c794";
+    }
+  });
+});
